044c3e65736634895dfbb9f020beae4f82abaeb4
pablocesar-sc <sandovalpablo069@gmail.com>
2024-11-28 21:05:37 -0500

    Update PC4_plantilla_blog.py

--- PC4_plantilla_blog.py
+++ PC4_plantilla_blog.py
@@ -60,7 +60,7 @@ col1, col2 = st.columns(2)
 # Las columnas creadas se asignan a las variables col1 y col2.
 
 # En la primera columna colocamos la imagen
-col1.image("prom_goles_de_visitante.jpeg", caption='terrible estadística', width=300)
+col1.image("Screenshot_20241128_205243.jpg", caption='foto de perfil', width=300)
 
 # col1.image("ellie.png", caption='Ellie', width=300): Esta línea está colocando una imagen en la primera columna (col1). 
 # La función image toma como primer argumento la ruta de la imagen que se va a mostrar. 
